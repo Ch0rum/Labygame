9f1c8cd783fb6a9c3fc6a0540c0d458d34d1f30c
Ch0rum <milan.bolak@gmail.com>
2026-04-29 12:46:59 +0200

    On a fini la fonction loadlevel, et les incrementations apres une victoire en press ENTER

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "raylib.h"
 #include <stdbool.h>
 
-// ENNEMISTRUCT
+// ENNEMIS
 typedef struct Enemy {
 	float startX;
 	float startY;
@@ -12,13 +12,78 @@ typedef struct Enemy {
 	float speed;
 	bool movesOnX;
 } Enemy;
-//ENNEMILISTE
-Enemy enemies[] = {
+
+// NIVEAUX
+typedef struct Level {
+	float playerStartX;
+	float playerStartY;
+	Rectangle exitRect;
+	Rectangle keyRect;
+	Rectangle doorRect;
+	Rectangle *walls;
+	int wallCount;
+	Enemy *enemies;
+	int enemyCount;
+
+} Level;
+
+// PROPRIETES DES MURS DE NIVEAU 1
+Rectangle level1walls[] = {
+	{0, 400, 175, 10},
+	{250, 400, 10, 200},
+	{60, 100, 740, 10},
+	{60, 100, 10, 250},
+	{250, 160, 10, 240}
+};
+
+// PROPRIETES DES MURS DE NIVEAU 2
+Rectangle level2walls[] = {
+	{50, 250, 10, 100},
+	{150, 200, 10, 100},
+	{600, 50, 10, 500},
+	{60, 100, 10, 250},
+	{150, 260, 10, 340}
+};
+
+// PROPRIETES DES ENNEMIS DE NIVEAU 1
+Enemy level1Enemies[] = {
 	//startx, starty,   X,      Y,    size,   startspeed,  speed      movesOnX
 	{300.0f, 530.0f, 300.0f, 530.0f, 20.0f,     200.0f,    200.0f,     true},
 	{100.0f, 200.0f, 100.0f, 200.0f, 20.0f,     150.0f,    150.0f,     false}
 };
 
+// PROPRIETES DES ENNEMIS DE NIVEAU 2
+Enemy level2Enemies[] = {
+	//startx, starty,   X,      Y,    size,   startspeed,  speed      movesOnX
+	{200.0f, 230.0f, 200.0f, 230.0f, 20.0f,     300.0f,    300.0f,     false},
+	{100.0f, 200.0f, 100.0f, 200.0f, 20.0f,     250.0f,    250.0f,     false}
+};
+
+// PROPRIETES DU NIVEAU 1
+Level level1 = {
+	740.0f,
+	40.0f,
+	{100, 500, 55, 55},
+	{750, 550, 30, 9},
+	{175, 400, 75, 10},
+	level1walls,
+	sizeof(level1walls) / sizeof(level1walls[0]),
+	level1Enemies,
+	sizeof(level1Enemies) / sizeof(level1Enemies[0])
+};
+
+// PROPRIETES DU NIVEAU 2
+Level level2 = {
+	500.0f,
+	200.0f,
+	{600, 300, 55, 55},
+	{150, 250, 30, 9},
+	{35, 200, 75, 10},
+	level2walls,
+	sizeof(level2walls) / sizeof(level2walls[0]),
+	level2Enemies,
+	sizeof(level2Enemies) / sizeof(level2Enemies[0])
+};
 
 // ETAT DE LA FENETRE
 typedef enum GameScreen {
@@ -28,7 +93,18 @@ typedef enum GameScreen {
 	GAMEOVER
 } GameScreen;
 
-
+void loadLevel(Level *currentLevel, float *playerStartX, float *playerStartY, Rectangle *exitRect, int *wallCount, Rectangle **walls, Rectangle *keyRect, Rectangle *doorRect, Enemy **enemies, int *enemyCount)
+{
+	*playerStartX = currentLevel->playerStartX;
+	*playerStartY = currentLevel->playerStartY;
+	*exitRect = currentLevel->exitRect;
+	*wallCount = currentLevel->wallCount;
+	*walls = currentLevel->walls;
+	*keyRect = currentLevel->keyRect;
+	*doorRect = currentLevel->doorRect;
+	*enemies = currentLevel->enemies;
+	*enemyCount = currentLevel->enemyCount;
+}
 
 bool movePlayer(float *playerX, float *playerY, float playerSpeed, float deltaTime, float playerSize, Rectangle walls[], int wallCount, bool hasKey, Rectangle doorRect)
 {
@@ -184,54 +260,51 @@ void restartLevel(float *playerX, float *playerY, float playerStartX, float play
 
 int main(void)
 {
+	// NIVEAU
+	Level levels[] = {level1, level2};
+	int levelCount = sizeof(levels) / sizeof(levels[0]);
+	int currentLevelIndex = 0;
+	Level *currentLevel = &levels[currentLevelIndex];
+
 	// JOUEUR
-	float playerStartX = 740;
-	float playerStartY = 40;
-	float playerX = playerStartX;
-	float playerY = playerStartY;
+	float playerStartX;
+	float playerStartY;
 	float playerSpeed = 200.0f;
 	float playerSize = 40.0f;
+
 	// SORTIE
-	int exitX = 100;
-	int exitY = 500;
-	int exitSizeX = 55;
-	int exitSizeY = 55;
-	Rectangle exitRect = {exitX, exitY, exitSizeX, exitSizeY};
+	Rectangle exitRect;
+
 	// ETAT DU JEU
 	bool win = false;
 	GameScreen currentScreen = TITLE;
 	float timer = 0.0f;
 	float bestTime = -1.0f;
-	// NIVEAU
-	Rectangle walls[] = {
-		//X, Y, WIDTH, HEIGHT
-		{0, 400, 175, 10},
-		{250, 400, 10, 200},
-		{60, 100, 740, 10},
-		{60, 100, 10, 250},
-		{250, 160, 10, 240},
-	};
-	int wallCount = sizeof(walls) / sizeof(walls[0]);
-	InitWindow(800, 600, "Mon premier jeu");
-	SetTargetFPS(60);
+
+	// NIVEAU 1
+	Rectangle *walls;
+	int wallCount;
 
 	// CLE
-	int keyX = 750;
-	int keyY = 550;
-	int keySizeX = 30;
-	int keySizeY = 9;
+	Rectangle keyRect;
 	bool hasKey = false;
-	Rectangle keyRect = {keyX, keyY, keySizeX, keySizeY};
 
 	// PORTE
-	int doorX = 175;
-	int doorY = 400;
-	int doorSizeX = 75;
-	int doorSizeY = 10;
-	Rectangle doorRect = {doorX, doorY, doorSizeX, doorSizeY};
-	// ENNEMI
-	int enemyCount = sizeof(enemies) / sizeof(enemies[0]);
+	Rectangle doorRect;
 
+	// ENNEMI
+	Enemy *enemies;
+	int enemyCount;
+
+
+	// CHARGER UN NIVEAU
+	loadLevel(currentLevel, &playerStartX, &playerStartY, &exitRect, &wallCount, &walls, &keyRect, &doorRect, &enemies, &enemyCount);
+
+	float playerX = playerStartX;
+	float playerY = playerStartY;
+
+	InitWindow(800, 600, "Mon premier jeu");
+	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
 		float deltaTime = GetFrameTime();
@@ -251,6 +324,7 @@ int main(void)
 			EndDrawing();
 			continue;
 		}
+
 		// ECRAN DE JEU
 		if (currentScreen == PLAYING)
 		{
@@ -268,9 +342,37 @@ int main(void)
 			}
 			blocked = false;
 		}
+
 		// ECRAN DE VICTOIRE
 		if (currentScreen == WIN)
 		{
+			if (IsKeyPressed(KEY_ENTER))
+			{
+				if (currentLevelIndex + 1 < levelCount)
+				{
+					currentLevelIndex++;
+					currentLevel = &levels[currentLevelIndex];
+
+					loadLevel(currentLevel,
+							&playerStartX,
+							&playerStartY,
+							&exitRect,
+							&wallCount,
+							&walls,
+							&keyRect,
+							&doorRect,
+							&enemies,
+							&enemyCount);
+
+					playerX = playerStartX;
+					playerY = playerStartY;
+					win = false;
+					timer = 0.0f;
+					hasKey = false;
+					currentScreen = PLAYING;
+					continue;
+				}
+			}
 			if (IsKeyPressed(KEY_R))
 			{
 				restartLevel(&playerX, &playerY, playerStartX, playerStartY, &win, &timer, &hasKey);
@@ -292,6 +394,7 @@ int main(void)
 			EndDrawing();
 			continue;
 		}
+
 		// ECRAN DE DEFAITE
 		if (currentScreen == GAMEOVER)
 		{
@@ -416,11 +519,6 @@ int main(void)
 			hasKey = true;
 		}
 
-		if (win && (bestTime < 0.0f || timer < bestTime))
-		{
-			bestTime = timer;
-		}
-
 		Color playerColor;
 		if (blocked)
 		{
@@ -443,7 +541,7 @@ int main(void)
 			Rectangle enemyRect = {enemies[i].x, enemies[i].y, enemies[i].size, enemies[i].size};
 			DrawRectangleRec(enemyRect, PURPLE);
 		}
-		DrawRectangle(exitX, exitY, exitSizeX, exitSizeY, YELLOW);
+		DrawRectangleRec(exitRect, YELLOW);
 		DrawRectangleRec(playerRect, playerColor);
 		drawHUD(win, wallCount, timer, bestTime);
 
